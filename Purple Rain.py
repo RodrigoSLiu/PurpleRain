bf1338c496c160cac0fcb6502bb223986494ac09
Rodrigo Shu Liu <70068342+RodrigoLiu1710@users.noreply.github.com>
2020-11-05 22:13:20 +0000

    Update Purple Rain.py
    Debugged

--- Purple Rain.py
+++ Purple Rain.py
@@ -1,16 +1,17 @@
 import pygame
 import random
+
 pygame.init()
 
-#window settings
+# window settings
 title = "Purple Rain"
 size = width, height = 640, 360
 
-#colors in rgb
+# colors in rgb
 background = (230, 230, 250)
 purple = (138, 43, 226)
 
-#initialization
+# initialization
 drops = []
 distance = {}
 speed = {}
@@ -18,70 +19,70 @@ clock = pygame.time.Clock()
 window = pygame.display.set_mode(size)
 pygame.display.set_caption(title)
 
-#classes
+
+# classes
 class Drop:
-	def __init__(self, x, y, z, speed, length, width):
-		self.x = x
-		self.y = y
-		self.z = z
-		self.speed = speed
-		self.len = length
-		self.width = width
-		self.gravity = 0.1
+    def __init__(self, x, y, z, dropSpeed, dropLen, dropWidth):
+        self.color = (138, 43, 226)
+        self.x = x
+        self.y = y
+        self.z = z
+        self.speed = dropSpeed
+        self.len = dropLen
+        self.width = dropWidth
+        self.gravity = 0.1
 
-	def fall(self):
-		self.y += self.speed
-		self.speed += self.gravity
-		if self.y > height:
-			self.y = random.randint(-500, -50)
-			z = random.randint(0, 20)
-			self.speed = speed[z]
+    def fall(self):
+        self.y += self.speed
+        self.speed += self.gravity
+        if self.y > height:
+            self.y = random.randint(-500, -50)
+            z = random.randint(0, 20)
+            self.speed = random.randint(0, 20)
 
-	def show(self):
-		self.color = (138, 43, 226)
-		pygame.draw.line(window, self.color, (self.x, int(self.y)), (self.x, int(self.y + self.len)), self.width)
-
+    def show(self):
+        pygame.draw.line(window, self.color, (self.x, int(self.y)), (self.x, int(self.y + self.len)), self.width)
 
 
 def create3D():
-	length = 10
-	speedDiff = 4
-	for i in range(21):
-		distance[i] = length
-		speed[i] = speedDiff
-		length += 0.5
-		speedDiff += 0.3
+    length = 10
+    speedDiff = 4
+    for i in range(21):
+        distance[i] = length
+        speed[i] = speedDiff
+        length += 0.5
+        speedDiff += 0.3
 
 
-#draws window
+# draws window
 def setup():
-	for i in range(550):
-		x = random.randint(0, 700)
-		y = random.randint(-500, -50)
-		z = random.randint(0, 20)
-		lineWidth = 1 if z < 14 else 2
-		drop = Drop(x, y, z, speed[z], distance[z], lineWidth)
-		drops.append(drop)
+    for i in range(550):
+        x = random.randint(0, 700)
+        y = random.randint(-500, -50)
+        z = random.randint(0, 20)
+        lineWidth = 1 if z < 14 else 2
+        drop = Drop(x, y, z, speed[z], distance[z], lineWidth)
+        drops.append(drop)
 
 
-#draw game window
+# draw game window
 def redrawGameWin():
-	window.fill(background)
-	for i in range(550):
-		drops[i].fall()
-		drops[i].show()
-	pygame.display.update()
+    window.fill(background)
+    for i in range(550):
+        drops[i].fall()
+        drops[i].show()
+    pygame.display.update()
 
 
-#main loop
+# main loop
 create3D()
 run = True
 while run:
-	setup()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
-	redrawGameWin()
-	clock.tick(70)
+    setup()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    redrawGameWin()
+    clock.tick(70)
 
 pygame.quit()
